e0dc3b19813bf59c74c376755f5a16e1eabdd1c7
Albear-Yang <albert.yang727@gmail.com>
2025-01-01 20:10:39 -0500

    Update FeaturedPost.tsx

--- src/components/FeaturedPost.tsx
+++ src/components/FeaturedPost.tsx
@@ -5,7 +5,7 @@ function FeaturedPost(){
         <div id="featuredPost">
             <div className="mainArticleBox">
             <Link to ="/About" className="link">
-                <h1 className="mainArticleTitle">About Albert Yang: 2A UW CS</h1>
+                <h1 className="mainArticleTitle">About Albert Yang: 2B UW CS</h1>
                 <p className="lead my-3">Hi! I'm a 2B University of Waterloo CS Student. This article will be talking about me and the purposes of this website. </p>
                 <p className="lead mb-0"><a id = "continueread">Continue reading...</a>
                 </p>
